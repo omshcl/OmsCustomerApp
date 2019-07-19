6d12f02937e3f9c7bcca39024ff06cac2284e2de
alicelambda <alicereuterdev@gmail.com>
2019-07-19 14:19:30 -0700

    Added Android base

---
+++ app/src/main/java/com/hcl/omsapplication/CreateOrder.java
@@ -0,0 +1,16 @@
+package com.hcl.omsapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateOrder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
